f4d7977e85664ecf9d72119841b7ce0edfef43a0
KvltKitty <jherzenach@mail.csuchico.edu>
2015-02-07 12:42:24 -0800

    Praise Kitty

--- src/Assets/Scripts/charController2D.cs
+++ src/Assets/Scripts/charController2D.cs
@@ -158,6 +158,12 @@ public class charController2D : MonoBehaviour {
 		processInput ();
 	}
 
+	public void handleSlope()
+	{
+
+
+	}
+
 	public void processInput()
 	{
 		_normalizedForce = new Vector2(0.0f, 0.0f);
--- src/Assets/Scripts/controllerState.cs
+++ src/Assets/Scripts/controllerState.cs
@@ -7,9 +7,11 @@ public class controllerState {
 	public bool isCollidingRight{get; set;}
 	public bool isCollidingLeft{get; set;}
 	public bool isEnteringDoor{get; set;}
+	public float slopeAngle{get; set;}
 
 	public void Reset(){
 		isCollidingAbove = isCollidingBelow = isCollidingRight = isCollidingLeft = isEnteringDoor = false;
+		slopeAngle = 0.0f;
 
 	}
 
