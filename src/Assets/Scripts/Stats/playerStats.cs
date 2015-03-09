222ece3d91466b908ace763a5a436f18e7e796cb
KvltKitty <jherzenach@mail.csuchico.edu>
2015-03-08 17:35:04 -0700

    Added variables
    Added array of abilities to playerStats.cs.  Also have it set the
    curAbility to an int.  Currently have it numbered 1-5 to make printing
    to the GUI easier, but this makes accessing it in the array
    abilities[num - 1]

--- src/Assets/Scripts/Stats/playerStats.cs
+++ src/Assets/Scripts/Stats/playerStats.cs
@@ -5,19 +5,15 @@ using System.Collections;
 public class playerStats {
 	public int hp;
 
-	public Ability atk1;
-	public Ability atk2;
-	public Ability atk3;
-	public Ability atk4;
-	public Ability atk5;
+	public Ability[] abilities = new Ability[5];
 
 	[HideInInspector]
 	public int curAbility;
 
 
-	public void loadAbilities(){
+	public void loadAbilities()
+	{
 		curAbility = 1;
-
 	}
 
 	public void printCurAbility(){
@@ -25,27 +21,7 @@ public class playerStats {
 	}
 
 	public void setAbility(int id, Ability ability){
-		if(id == 1){
-			atk1 = ability;
-			return;
-		}
-		if(id == 2){
-			atk2 = ability;
-			return;
-		}
-		if(id == 3){
-			atk3 = ability;
-			return;
-		}
-		if(id == 4){
-			atk4 = ability;
-			return;
-		}
-		if(id == 5){
-			atk5 = ability;
-			return;
-		}
-
+		abilities[id - 1] = ability;
 	}
 
 
