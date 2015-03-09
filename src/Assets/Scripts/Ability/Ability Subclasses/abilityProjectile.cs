8740ce8ba12f800121f9ced9058fe57450be5bf5
KvltKitty <jherzenach@mail.csuchico.edu>
2015-03-09 15:06:06 -0700

    Added comments and TODO's to projectile subclass

--- src/Assets/Scripts/Ability/Ability Subclasses/abilityProjectile.cs
+++ src/Assets/Scripts/Ability/Ability Subclasses/abilityProjectile.cs
@@ -27,12 +27,19 @@ public class abilityProjectile : Ability {
 		{
 			if(_numProjectiles == 1){
 				GameObject newInstace = MonoBehaviour.Instantiate (_projectile, castPoint, Quaternion.identity) as GameObject;
+				//TODO: Send message of type abilityProjectile to a setter function within the projectile
+
+
 				cdEndTime = Time.time + cdTimer;
 				onCD = true;
 			}
 			else
 			{
-				//TODO: Need to get the boxcollider width and evenly distribute the projectiles in front of the players
+				/*TODO: Need to get the boxcollider width and evenly distribute the projectiles in front of the players
+				 * unless we want to have more interesting shapes.  I think perhaps allowing the player to choose the angles
+				 * before a challenge could add another level of depth to the strategy
+				 * May be a bit more difficult to implement, but not by much.  Could essentially just have an array of bools for each possible castposition from say, 1-8, that each cast would iterate through
+				 * or maybe just labeling the positions 1-8, and then having an array of castPoint would be good, so it essentially just iterates through each and passes them the info they need */
 			}
 		}
 	}
