47e15443bffd7cf84ab47bda46f1fee60b400346
KvltKitty <jherzenach@mail.csuchico.edu>
2015-04-11 17:22:10 -0700

    Started on Projectile_Attack

--- src/Assets/Scripts/Ability/Ability Subclasses/abilityProjectile.cs
+++ src/Assets/Scripts/Ability/Ability Subclasses/abilityProjectile.cs
@@ -11,7 +11,6 @@ using System.Collections;
  */
 
 public class abilityProjectile : Ability {
-	GameObject projectile;
 
 	float[] damage_array = new float[5]{2.0f, 5.0f, 8.0f, 15.0f, 30.0f};
 	float[] speed_array = new float[5]{1.0f, 1.5f, 2.0f, 2.5f, 3.0f};
@@ -46,6 +45,9 @@ public class abilityProjectile : Ability {
 		this.explosive = false;
 		this.lingering = false;
 
+		this.cdTimer = 1.0f;
+		this.cdEndTime = 0.0f;
+
 		if (this._debug) {
 			Debug.Log ("Projectile Ability instanciated!");
 		}
@@ -58,27 +60,15 @@ public class abilityProjectile : Ability {
 
 	public override void Cast(Vector3 castPoint)
 	{
-		if(!onCD)
+		if(Time.time >= cdEndTime)
 		{
 			Debug.Log("Cast called for " + this.getName());
-			/*if(projectiles == 1){
-				GameObject newInstace = MonoBehaviour.Instantiate (projectile, castPoint, Quaternion.identity) as GameObject;
-				//TODO: Send message of type abilityProjectile to a setter function within the projectile
-
-
-				cdEndTime = Time.time + cdTimer;
-				onCD = true;
-			}
-			else
-			{
-				/*TODO: Need to get the boxcollider width and evenly distribute the projectiles in front of the players
-				 * unless we want to have more interesting shapes.  I think perhaps allowing the player to choose the angles
-				 * before a challenge could add another level of depth to the strategy
-				 * May be a bit more difficult to implement, but not by much.  Could essentially just have an array of bools for each possible castposition from say, 1-8, that each cast would iterate through
-				 * or maybe just labeling the positions 1-8, and then having an array of castPoint would be good, so it essentially just iterates through each and passes them the info they need 
-				 * We could even incorporate both, perhaps have a dropdown option that lets them choose if the projectiles are unidirectional or not
-				 */
-			//}
+			GameObject newInstance = MonoBehaviour.Instantiate (Resources.Load ("Projectile_Attack"), castPoint, Quaternion.identity) as GameObject;
+			newInstance.transform.position += newInstance.transform.up * (newInstance.GetComponent<BoxCollider2D>().size.y/2);
+			//TODO: Send message of type abilityProjectile to a setter function within the projectile
+			Projectile_Attack script = newInstance.GetComponent<Projectile_Attack>();
+			script.init(this);
+			cdEndTime = Time.time + cdTimer;
 		}
 	}
 
--- src/Assets/Scripts/Ability/Projectile_Attack.cs
+++ src/Assets/Scripts/Ability/Projectile_Attack.cs
@@ -5,14 +5,53 @@ public class Projectile_Attack : MonoBehaviour {
 	public Ability ability;
 	private GameObject player;
 	
-	public GameObject projectile;
+	public SpriteRenderer projectile;
+	GameObject temp;
 
 	
-	void Start () {
+	void Awake () {
+		Debug.Log ("Call start");
 		this.player = GameObject.Find("player_control");
+		transform.rotation = player.transform.GetChild(0).transform.GetChild(0).transform.rotation;
+		projectile = gameObject.GetComponent <SpriteRenderer>();
+
 	}
 
 	void Update () {
-		
+		if (ability == null)return;
 	}
+
+	public void init(Ability ability)
+	{
+		Debug.Log ("call init");
+		this.ability = ability;
+
+		string element = ability.getElementString();
+		switch (element) {
+		case("Fire"):
+			temp = Resources.Load<GameObject>("projectile_fire");
+			projectile.sprite = temp.GetComponent<SpriteRenderer>().sprite;
+			break;
+			
+		case("Frost"):
+			temp = Resources.Load<GameObject>("projectile_frost");
+			projectile.sprite = temp.GetComponent<SpriteRenderer>().sprite;
+            break;
+			
+		case("Poison"):
+			temp = Resources.Load<GameObject>("projectile_poison");
+			projectile.sprite = temp.GetComponent<SpriteRenderer>().sprite;
+            break;
+			
+		case("Electric"):
+			temp = Resources.Load<GameObject>("projectile_fire");
+			projectile.sprite = temp.GetComponent<SpriteRenderer>().sprite;
+            break;
+                
+        case("Dark"):
+			temp = Resources.Load<GameObject>("projectile_dark");
+			projectile.sprite = temp.GetComponent<SpriteRenderer>().sprite;
+            break;
+        }
+    }
 }
