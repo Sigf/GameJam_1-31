f2302d925d49b0b3046702945c8743f5fefbb5df
KvltKitty <jherzenach@mail.csuchico.edu>
2015-04-27 10:50:11 -0700

    fixed some wonky stuff
    Just some annoying error messaged and a debug logerror that came up too
    much.  Also made the creation of the fixed point child more consistent
    and not dependent on the num of children, but rather a locally created
    variable in the editor

--- src/Assets/Scripts/AI/obstacleEditor.cs
+++ src/Assets/Scripts/AI/obstacleEditor.cs
@@ -6,7 +6,7 @@ using UnityEditor;
 [CustomEditor(typeof(obstacleAI))]
 public class obstacleEditor : Editor 
 {
-	private bool fixedPointCreated ;
+	private bool fixedPointCreated = false;
 	private obstacleAI _AI;
 	
 	public override void OnInspectorGUI(){
@@ -23,20 +23,19 @@ public class obstacleEditor : Editor
 			OnClearShotOptions();
 		}
 		if(_AI.parameters.whenFire == Obstacle.fireOn.onCollision){
-			_AI.parameters.triggerTag = EditorGUILayout.TextField("Trigger Tag", _AI.parameters.triggerTag);
+			OnCollisionOptions ();
 		}
 
-
 		if(_AI.parameters.whereAim == Obstacle.aimTowards.fixedPoint){
-			if(_AI.transform.childCount < 1){
+			if(!fixedPointCreated){
 				CreateFixedPoint ();
 			}
 		}
-		if(_AI.parameters.whereAim != Obstacle.aimTowards.fixedPoint){
-			Transform findChild = _AI.transform.FindChild(_AI.transform.name + " Firing Point");
-			if(findChild != null){
-			DestroyImmediate (findChild.gameObject);
-			}
+		if(_AI.parameters.whereAim != Obstacle.aimTowards.fixedPoint && fixedPointCreated){
+			DestroyFixedPoint();
+		}
+		if(GUI.changed){
+			EditorUtility.SetDirty ((obstacleAI)target);
 		}
 
 	}
@@ -53,13 +52,13 @@ public class obstacleEditor : Editor
 		_AI.parameters.cdTime = EditorGUILayout.FloatField("CD Time", _AI.parameters.cdTime);
 		_AI.parameters.whereAim = (Obstacle.aimTowards)EditorGUILayout.EnumPopup ("Fire Towards", _AI.parameters.whereAim);
 		if(_AI.parameters.whereAim == Obstacle.aimTowards.front){
-			Debug.LogError ("Cannot Choose Front When Firing On Clear Shot");
 			_AI.parameters.whereAim = Obstacle.aimTowards.player;
 		}
 	}
 
 	public void OnCollisionOptions(){
-
+		_AI.parameters.cdTime = EditorGUILayout.FloatField("CD Time", _AI.parameters.cdTime);
+		_AI.parameters.triggerTag = EditorGUILayout.TextField("Trigger Tag", _AI.parameters.triggerTag);
 	}
 
 	public void CreateFixedPoint(){
@@ -67,10 +66,16 @@ public class obstacleEditor : Editor
 		fixedPoint.transform.parent = _AI.transform;
 		fixedPoint.name = (_AI.transform.name + " Firing Point");
 		fixedPoint.transform.position = _AI.transform.position;
+		_AI.parameters.firePoint = fixedPoint;
+		fixedPointCreated = true;
 	}
 
 	public void DestroyFixedPoint(){
-
+		Transform findChild = _AI.transform.FindChild(_AI.transform.name + " Firing Point");
+		if(findChild != null){
+			DestroyImmediate (findChild.gameObject);
+		}
+		fixedPointCreated = false;
 	}
 
 
